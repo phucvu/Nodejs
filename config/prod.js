9cade87672490fd68d7f3c19b859897b52b7d09e
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2018-11-26 09:50:55 +0700

    save prod config to heroku setting

--- config/prod.js
+++ config/prod.js
@@ -8,14 +8,3 @@ module.exports = {
   mongoPassword: process.env.MONGO_PASSWORD,
   cookieKey: process.env.COOKIE_KEY
 };
-
-// sample
-//   module.exports = {
-//     googleClientID: '30521372967-ustq5a8bn8qu05om407733ftheoch56d.apps.googleusercontent.com',
-//     googleClientSecret: 'LNtgZwqTYNzqLlwIB6zayzKx',
-//     mongoURI: 'mongodb://phucvu:123456q@ds123151.mlab.com:23151/emaily-dev-ph',
-//     mongoURL: 'mongodb://ds115154.mlab.com:15154/emaily-pro-ph',
-//     mongoUsername: 'phucvu',
-//     mongoPassword: '123456q',
-//     cookieKey: 'hxiasla;dlaoem932mnx329nad;29ndaf'
-//   };
